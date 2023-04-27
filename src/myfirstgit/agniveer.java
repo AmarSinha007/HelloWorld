a49c4b174ec37407f8486aa6d35d261028c795c6
unknown <amarsinha@007>
2023-04-27 15:46:17 +0530

    src code

---
+++ src/myfirstgit/agniveer.java
@@ -0,0 +1,7 @@
+package myfirstgit;
+
+public class agniveer {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("agniveer");}
+}
---
+++ src/myfirstgit/amar.java
@@ -0,0 +1,9 @@
+package myfirstgit;
+
+public class amar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("sfjsf jh jfds fj sd");}
+
+}
