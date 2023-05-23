cdb71fcdc182fa063628dff438939b4f4e9a7c11
unknown <amarsinha@007>
2023-05-23 10:16:53 +0530

    PrimeNum

--- src/myfirstgit/PrimeNum.java
+++ src/myfirstgit/PrimeNum.java
@@ -1,11 +1,24 @@
 package myfirstgit;
 
 public class PrimeNum {
-public static void main (String []args) {
-	
-	for(int i= 1; i<= 100;i++)
-		if(i%1==0) {
-		System.out.println(i);	
-		}
-}
-}
+	    public static void main(String[] args) {
+	       
+	        for (int i = 2; i <= 100; i++) {
+	            boolean isPrime = true;
+
+	            for (int j = 2; j < i; j++) {
+	                if (i % j == 0) {
+	                    isPrime = false;
+	                    break;
+	                }
+	            }
+
+	            if (isPrime) {
+	                System.out.print(i + " ");
+	            }
+	        }
+	    }
+	}
+
+
+
